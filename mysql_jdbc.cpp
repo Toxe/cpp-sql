deba0a6c0e55d26bbacdd483f636406990124b20
Toxe <toxe@toxe.net>
2019-07-08 16:28:14 +0200

    std::unique_ptr verwenden

--- mysql_jdbc.cpp
+++ mysql_jdbc.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 #include <mysql/jdbc.h>
 
 int main()
@@ -8,23 +9,18 @@ int main()
     connection_properties["socket"]          = "/Applications/MAMP/tmp/mysql/mysql.sock";
 
     sql::mysql::MySQL_Driver* driver = sql::mysql::get_driver_instance();
-    sql::Connection* con = driver->connect(connection_properties);
+    std::unique_ptr<sql::Connection> con(driver->connect(connection_properties));
+    std::unique_ptr<sql::Statement> stmt(con->createStatement());
 
-    sql::Statement* stmt = con->createStatement();
     stmt->execute("USE contiview");
+    std::unique_ptr<sql::ResultSet> res(stmt->executeQuery("SELECT * FROM content_object ORDER BY id LIMIT 10"));
 
     std::cout << "id" << "\t" << "parent" << "\t" << "type" << std::endl;
     std::cout << "----------------------------" << std::endl;
-
-    sql::ResultSet* res = stmt->executeQuery("SELECT * FROM content_object ORDER BY id LIMIT 10");
 
     while (res->next()) {
         std::cout << res->getString("id")     << "\t"
                   << res->getString("parent") << "\t"
                   << res->getString("type")   << std::endl;
     }
-
-    delete res;
-    delete stmt;
-    delete con;
 }
