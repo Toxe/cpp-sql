9327c17083a13bffcd83db5359e7a4acab643311
Toxe <toxe@toxe.net>
2020-03-09 20:44:37 +0100

    Add sqlpp11_mysql.cpp

---
+++ ships.ddl
@@ -0,0 +1,61 @@
+-- MySQL dump 10.13  Distrib 5.7.26, for osx10.9 (x86_64)
+--
+-- Host: localhost    Database: ships
+-- ------------------------------------------------------
+-- Server version	5.7.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ship_roles`
+--
+
+DROP TABLE IF EXISTS `ship_roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ship_roles` (
+  `id` int(11) NOT NULL,
+  `role` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`,`role`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ships`
+--
+
+DROP TABLE IF EXISTS `ships`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ships` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `length` int(11) NOT NULL,
+  `crew` int(11) NOT NULL,
+  `affiliation` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `category` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `manufacturer` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `model` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `ship_class` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-03-09 14:06:55
---
+++ sqlpp11_mysql.cpp
@@ -0,0 +1,114 @@
+#include <fstream>
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <sqlpp11/sqlpp11.h>
+#include <sqlpp11/mysql/mysql.h>
+#include "ships.h"
+
+// Read JSON config file.
+//
+// Example "mysql_conf.json":
+//
+//   {
+//       "user": "username",
+//       "password": "password",
+//       "database": "ships",
+//       "unix_socket": "/tmp/mysql.sock"
+//   }
+std::shared_ptr<sqlpp::mysql::connection_config> read_mysql_config(const char* filename)
+{
+    std::ifstream in(filename);
+    nlohmann::json data;
+
+    in >> data;
+    auto config = std::make_shared<sqlpp::mysql::connection_config>();
+
+    if (!data["host"].empty()) config->host = data["host"].get<std::string>();
+    if (!data["user"].empty()) config->user = data["user"].get<std::string>();
+    if (!data["password"].empty()) config->password = data["password"].get<std::string>();
+    if (!data["database"].empty()) config->database = data["database"].get<std::string>();
+    if (!data["unix_socket"].empty()) config->unix_socket = data["unix_socket"].get<std::string>();
+    if (!data["charset"].empty()) config->charset = data["charset"].get<std::string>();
+    if (!data["port"].empty()) config->port = data["port"].get<unsigned int>();
+    if (!data["client_flag"].empty()) config->client_flag = data["client_flag"].get<unsigned long>();
+    if (!data["auto_reconnect"].empty()) config->auto_reconnect = data["auto_reconnect"].get<bool>();
+    if (!data["debug"].empty()) config->debug = data["debug"].get<bool>();
+
+    return config;
+}
+
+SQLPP_ALIAS_PROVIDER(feet);
+SQLPP_ALIAS_PROVIDER(col_count);
+
+int main()
+{
+    auto config = read_mysql_config("mysql_conf.json");
+    sqlpp::mysql::connection db(config);
+
+    Ships::Ships ships;
+    Ships::ShipRoles shipRoles;
+
+    std::cout << "\n----------------------------------------------------------------------------\n"
+              << "SELECT * FROM ships"
+              << "\n----------------------------------------------------------------------------\n";
+
+    for (const auto& row : db(select(all_of(ships)).from(ships).unconditionally())) {
+        std::cout << row.id << ": " << row.length << ", " << row.crew << ", " << row.affiliation << ", " << row.category << ", " << row.manufacturer << ", " << row.model << ", " << row.shipClass << '\n';
+    }
+
+    std::cout << "\n----------------------------------------------------------------------------\n"
+              << "SELECT id, model FROM ships ORDER BY model DESC LIMIT 5"
+              << "\n----------------------------------------------------------------------------\n";
+
+    for (const auto& row : db(select(ships.id, ships.model).from(ships).unconditionally().order_by(ships.model.desc()).limit(5u))) {
+        std::cout << row.id << ": " << row.model << '\n';
+    }
+
+    std::cout << "\n----------------------------------------------------------------------------\n"
+              << "SELECT id, category, model FROM ships WHERE category = 'Starfighters'"
+              << "\n----------------------------------------------------------------------------\n";
+
+    for (const auto& row : db(select(ships.id, ships.category, ships.model).from(ships).where(ships.category == "Starfighters"))) {
+        std::cout << row.id << ": " << row.category << ", " << row.model << '\n';
+    }
+
+    std::cout << "\n----------------------------------------------------------------------------\n"
+              << "SELECT id, model, length FROM ships WHERE length >= 1000 AND model LIKE '%Destroyer%'"
+              << "\n----------------------------------------------------------------------------\n";
+
+    for (const auto& row : db(select(ships.id, ships.model, ships.length).from(ships).where(ships.length >= 1000 && ships.model.like("%Destroyer%")))) {
+        std::cout << row.id << ": " << row.model << " = " << row.length << '\n';
+    }
+
+    std::cout << "\n----------------------------------------------------------------------------\n"
+              << "SELECT id, model, length AS meters, length * 3.2808 AS feet FROM ships WHERE length <= 20"
+              << "\n----------------------------------------------------------------------------\n";
+
+    for (const auto& row : db(select(ships.id, ships.model, ships.length, (ships.length * 3.2808).as(feet)).from(ships).where(ships.length <= 20))) {
+        std::cout << row.id << ": " << row.model << " = " << row.length << " meters (or " << row.feet << " feet)\n";
+    }
+
+    std::cout << "\n----------------------------------------------------------------------------\n"
+              << "SELECT DISTINCT category FROM ships ORDER BY category ASC"
+              << "\n----------------------------------------------------------------------------\n";
+
+    for (const auto& row : db(select(ships.category).flags(sqlpp::distinct).from(ships).unconditionally().order_by(ships.category.asc()))) {
+        std::cout << row.category << '\n';
+    }
+
+    std::cout << "\n----------------------------------------------------------------------------\n"
+              << "SELECT ship_class, COUNT(*) AS `col_count` FROM ships GROUP BY ship_class"
+              << "\n----------------------------------------------------------------------------\n";
+
+    for (const auto& row : db(select(ships.shipClass, count(ships.shipClass).as(col_count)).from(ships).unconditionally().group_by(ships.shipClass))) {
+        std::cout << row.shipClass << ": " << row.col_count << '\n';
+    }
+
+    std::cout << "\n----------------------------------------------------------------------------\n"
+              << "SELECT ships.id, ships.model, ship_roles.role FROM ships JOIN ship_roles ON ships.id = ship_roles.id ORDER BY id ASC, ship_roles.role ASC LIMIT 10"
+              << "\n----------------------------------------------------------------------------\n";
+
+    for (const auto& row : db(select(ships.id, ships.model, shipRoles.role).from(ships.join(shipRoles).on(ships.id == shipRoles.id)).unconditionally().order_by(ships.id.asc(), shipRoles.role.asc()).limit(10u))) {
+        std::cout << row.id << ": " << row.model << " = " << row.role << '\n';
+    }
+}
