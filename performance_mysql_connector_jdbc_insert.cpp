fe5a8978088e4347934d7d6b5bf94801f760011e
Toxe <toxe@toxe.net>
2020-03-18 20:11:04 +0100

    Remove single INSERTs test

--- performance_mysql_connector_jdbc_insert.cpp
+++ performance_mysql_connector_jdbc_insert.cpp
@@ -52,37 +52,6 @@ sql::ConnectOptionsMap read_mysql_config(const char* filename)
     return connection_properties;
 }
 
-void import_data_single_inserts(sql::Connection* con, const std::string& table_name, const std::string& filename)
-{
-    std::cout << "* Import data (single INSERTs)...\n";
-
-    std::ifstream in{filename};
-    int count = 0;
-    auto t0 = std::chrono::high_resolution_clock::now();
-
-    std::unique_ptr<sql::Statement> stmt(con->createStatement());
-
-    while (in) {
-        std::string s1;  if (!std::getline(in, s1, '\t')) break;
-        std::string s2;  if (!std::getline(in, s2, '\t')) break;
-        std::string s3;  if (!std::getline(in, s3, '\t')) break;
-        std::string s4;  if (!std::getline(in, s4, '\t')) break;
-        std::string s5;  if (!std::getline(in, s5, '\t')) break;
-        std::string s6;  if (!std::getline(in, s6, '\t')) break;
-        std::string s7;  if (!std::getline(in, s7))       break;
-
-        std::string sql{"INSERT INTO " + table_name + " (time, request, duration, user, ip_address, action, user_agent) VALUES " +
-                        "(\"" + s1 + "\"," + s2 + "," + s3 + "," + s4 + ",\"" + s5 + "\",\"" + s6 + "\",\"" + s7 + "\")"};
-        stmt->execute(sql);
-        ++count;
-    }
-
-    stmt.release();
-
-    auto t1 = std::chrono::high_resolution_clock::now();
-    std::cout << "  --> Imported " << count << " rows in " << std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count() << " ms\n";
-}
-
 struct Dataset {
     Dataset(std::ifstream& in) {
         if (!std::getline(in, s1, '\t')) throw std::runtime_error{"misformed input data"};
@@ -205,10 +174,6 @@ int main(int argc, char* argv[])
     std::unique_ptr<sql::Connection> con{connect(connection_properties)};
 
     warmup(performance_data_filename);
-
-    drop_table(con.get(), "performance");
-    create_table(con.get(), "performance");
-    import_data_single_inserts(con.get(), "performance", performance_data_filename);
 
     drop_table(con.get(), "performance");
     create_table(con.get(), "performance");
